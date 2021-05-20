4d52a9a028c27fa8e06e85822c487452355d871d
bypark <iblue@naver.com>
2021-05-20 11:49:40 +0900

    aws-exports.js added

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ import Amplify from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
 
-ReactDOM.render( <
-    React.StrictMode >
-    <
-    App / >
-    <
-    /React.StrictMode>,
-    document.getElementById('root')
+ReactDOM.render( 
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
